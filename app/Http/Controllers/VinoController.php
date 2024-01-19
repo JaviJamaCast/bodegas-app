4518538920f8723ec132cd2b00a9b49d05eff7f3
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-01-19 13:05:53 +0100

    Bodegas init

---
+++ app/Http/Controllers/VinoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vino;
+use Illuminate\Http\Request;
+
+class VinoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vino $vino)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vino $vino)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vino $vino)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vino $vino)
+    {
+        //
+    }
+}
---
+++ resources/views/bodegas/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bodegas</title>
+</head>
+
+<body>
+    <h1>Lista de bodegas</h1>
+</body>
+
+</html>
